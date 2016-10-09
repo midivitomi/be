5807b290a7bd0f314c87144cbb288d276089c173
Vitaly Korneev <redeffect.by@gmail.com>
2016-10-09 14:51:08 +0300

    add matches api

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,3 +1,10 @@
 class MatchesController < ApplicationController
-  
+  def index
+    competitions = params[:competitions].split(',')
+
+    matches = Matche.where(group_id: competitions)
+
+    render json: matches
+
+  end
 end
